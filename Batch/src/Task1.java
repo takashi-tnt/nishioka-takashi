71f586331eb765b238a2d084f90880fe1695a785
takashi-tnt <nishi.tion.z.takanishi0641@gmail.com>
2020-02-29 17:02:51 +0900

    Javaの基礎の課題です。

---
+++ Batch/src/Task1.java
@@ -0,0 +1,10 @@
+
+public class Task1 {
+
+	public static void main(String[] args) {
+		System.out.println("初めての課題");
+		System.out.println("Java攻略の道");
+
+	}
+
+}
---
+++ Batch/src/Task2.java
@@ -0,0 +1,12 @@
+
+public class Task2 {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("hogehoge");
+		System.out.println("hogehoge2");
+
+	}
+	//上のmainの{}を出ると表示されなくなるため、中に入れる。
+
+}
