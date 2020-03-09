4de40dc9425c86aa1cb35bf1ae710a466e292252
takashi-tnt <nishi.tion.z.takanishi0641@gmail.com>
2020-03-09 14:42:56 +0900

    小修整

--- method/src/Task4.java
+++ method/src/Task4.java
@@ -4,12 +4,12 @@ public class Task4 {
 	public static void main(String[] args) {
 		String[] array1 = {"a","b","c"};
 		String[] array2 = {"a", null, "c"};
-		boolean answer1 = nullCheck(array1);
-		boolean answer2 = nullCheck(array2);
+
 		System.out.println("--配列にnullがない場合--");
-		System.out.println(answer1);
+		System.out.println(nullCheck(array1));
+
 		System.out.println("--配列にnullがある場合--");
-		System.out.println(answer2);
+		System.out.println(nullCheck(array2));
 
 
 	}
