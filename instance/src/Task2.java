0c13208f4d8939b8adbb9a5020bcf1afd9dfef59
takashi-tnt <nishi.tion.z.takanishi0641@gmail.com>
2020-03-21 18:46:37 +0900

    再提出-クラスとインスタンスの課題

--- instance/src/Task2.java
+++ instance/src/Task2.java
@@ -2,7 +2,9 @@
 public class Task2 {
 
 	public static void main(String[] args) {
+		//インスタンスの生成
 		Person person = new Person();
+		//インスタンス変数への代入
 		person.name = "西岡　隆志";
 		person.age = 26;
 	}
--- instance/src/Task3.java
+++ instance/src/Task3.java
@@ -2,9 +2,12 @@
 public class Task3 {
 
 	public static void main(String[] args) {
+		//インスタンスの生成
 		Person person = new Person();
+		//インスタンス変数への代入
 		person.name = "西岡　隆志";
 		person.age = 26;
+		//インスタンスメソッドの実行
 		person.myProfile();
 	}
 
