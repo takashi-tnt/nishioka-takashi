55999c408e5ca2e1915a5951fe21b3c0bc1df917
takashi-tnt <nishi.tion.z.takanishi0641@gmail.com>
2020-03-21 18:48:13 +0900

    再提出-標準クラスの課題

--- standardClass/src/Task7.java
+++ standardClass/src/Task7.java
@@ -4,12 +4,15 @@ public class Task7 {
 
 	public static void main(String[] args) {
 		Calendar cal = Calendar.getInstance();
+		//SimpleDateForMatを使用する場合
 		SimpleDateFormat f =new SimpleDateFormat("yyyy年MM月dd日");
 	    String s = f.format(cal.getTime());
 	    System.out.println("現在日時: " + s);
+	    //一週間後
 	    cal.add(Calendar.DAY_OF_MONTH, 7);
 	    s = f.format(cal.getTime());
 	    System.out.println("一週間後: " + s);
+	    //一年後
 	    cal.add(Calendar.YEAR, 1);
 		cal.add(Calendar.DAY_OF_MONTH, -7);
 		s = f.format(cal.getTime());
